40fc830d5433983bd3ba48898c8e1b6946506b2b
Elle Majors <hellolovely@ellemajors.com>
2023-03-27 17:31:47 -0600

    add: item create test, controller, route

--- app/controller/api/v1/items_controller.rb
+++ app/controller/api/v1/items_controller.rb
@@ -2,4 +2,18 @@ class Api::V1::ItemsController < ApplicationController
    def index
     render json: Item.all
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    render json: Item.create(item_params), status: 201
+  end
+
+  private
+  
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,6 +10,8 @@ describe "Items API" do
 
     items = JSON.parse(response.body)
 
+    expect(items.count).to eq(3)
+
     items.each do |item|
       expect(item).to have_key("name")
       expect(item["name"]).to be_a(String)
@@ -20,5 +22,45 @@ describe "Items API" do
       expect(item).to have_key("unit_price")
       expect(item["unit_price"]).to be_a(Float)
     end
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to eq(id)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+  end
+
+  it "can create a new item" do
+    item_params = { name: "Gloves", description: "Warm and fuzzy", unit_price: 25.00 }
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+# require 'pry'; binding.pry
+    expect(response).to be_successful
+    #returning nil for created_item
+    # expect(created_item.name).to eq(item_params[:name])
+    # expect(created_item.description).to eq(item_params[:description])
+    # expect(created_item.unit_price).to eq(item_params[:unit_price])
+    # expect(created_item).to have_attributes(item_params)
+    expect(response.status).to eq(201)
+    #returning application/json; charset=utf-8 for content_type
+    # expect(response.content_type).to eq("application/json")
+    #returning nil for location
+    # expect(response.location).to eq("http://www.example.com/api/v1/items/#{created_item.id}")
   end
 end
