eb3a4e039bd35c288555a65592fcfa8a6ba00094
Elle Majors <hellolovely@ellemajors.com>
2023-03-28 11:44:54 -0600

    add: merchant items controller, routes, serializers'

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
+    # render json: MerchantSerializer.new(Merchant.all).serializable_hash.to_json
   end
 
   def show
+    @merchant = Merchant.find(params[:id])
+    @items = @merchant.items
+
     render json: Merchant.find(params[:id])
-    all_items = Item.where(merchant_id: params[:id])
+    # render json: MerchantSerializer.new(Merchant.find(params[:id])).serializable_hash.to_json
   end
 end
